4be3c3fd86fcdd87f31e5b9d9485515a903c541e
talhakhalil0703 <tkhalil0703@gmail.com>
2019-10-06 18:16:28 -0600

    Functionality to add one PDF document as attachment

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ USER = os.environ.get('GMAIL_USER')
 PASS = os.environ.get('GMAIL_PASS')
 RECIEVER = USER
 
+readFile = None
+fileName = None
+
 first_names = []
 last_names = []
 emails = []
@@ -22,11 +25,15 @@ window.title("Mass Email")
 data_path = Entry(window)
 subject_entry = Entry(window)
 data_path.grid(column = 0, columnspan = 2, row = 0)
+
 subject_entry.grid(column = 0, columnspan = 2, row = 1)
-subject_entry.insert(END, "Subject")
+subject_entry.insert(END, "Digitronics: ")
+
+attachment_entry = Entry(window)
+attachment_entry.grid(column = 0, columnspan = 2, row = 2)
 
 body = scrolledtext.ScrolledText(window, height = 20)
-body.grid(column = 0, columnspan = 4, row = 2)
+body.grid(column = 0, columnspan = 4, row = 3)
 body.insert(END, 'Thanks, \n\nDigitronics Team')
 
 def find_information():
@@ -56,6 +63,9 @@ def send_emails():
             msg['To'] = RECIEVER
             content = "Hi " + name.capitalize() + ", \n" + body.get(1.0, END)
             msg.set_content(content)
+            global readFile, fileName
+            if (fileName != None):
+                msg.add_attachment(readFile, maintype = "application", subtype= "octet-stream", filename = fileName)
 
             with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
                 smtp.ehlo()
@@ -63,7 +73,6 @@ def send_emails():
                 smtp.ehlo()
 
                 smtp.login(USER, PASS)
-
                 smtp.send_message(msg)
                 print("Email sent to: ", RECIEVER)
 
@@ -71,6 +80,16 @@ def browse_clicked():
     window.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("CSV files","*.csv"), ("all files","*.*")))
     data_path.delete(0, END)
     data_path.insert(END, window.filename)
+
+def attachment_browse_clicked():
+    window.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("PDF files","*.pdf"), ("all files","*.*")))
+    attachment_entry.delete(0, END)
+    attachment_entry.insert(END, window.filename)   
+    global readFile, fileName
+    with open (attachment_entry.get(), "rb") as f:
+        readFile = f.read()
+        fileName = f.name
+
 
 def exit_clicked():
     sys.exit(0)
@@ -82,12 +101,15 @@ def send_email_clicked():
     print(body.get(1.0, END))
 
 load_csv_button = Button(window, text = 'LOAD EMAIL CSV', command = browse_clicked)
-send_email_buttonn = Button(window, text = 'SEND EMAILS', command = send_email_clicked)
+load_attachment_button = Button(window, text = 'LOAD ATTACHMENTS', command = attachment_browse_clicked)
+
+send_email_button = Button(window, text = 'SEND EMAILS', command = send_email_clicked)
 exit_button = Button(window, text = 'CLOSE', fg ='red', command = exit_clicked)
 
 
 load_csv_button.grid(column = 3, row = 0)
-send_email_buttonn.grid(column = 0, row = 3)
-exit_button.grid(column = 3, row = 3)
+load_attachment_button.grid(column = 3, row = 2)
+send_email_button.grid(column = 0, row = 4)
+exit_button.grid(column = 3, row = 4)
 
 window.mainloop()
